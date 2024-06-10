912a6eefc6bdc1fd6f7b698b7b251b511c29d769
kawk10 <wonyeongwonyeong@gmail.com>
2024-06-10 22:37:32 +0900

    값 보내기 test

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Route, BrowserRouter, Routes} from 'react-router-dom'
+import TestSend from './pages/TestSend.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/test" element={<TestSend />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
